c34282372d2313c161f299c98b81e3ba026dd66d
anaasima <58686057+anaasima@users.noreply.github.com>
2021-04-14 09:32:55 +0200

    hey

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatabaseSEP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
